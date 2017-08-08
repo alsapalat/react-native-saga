e95aedabf131407673fcd9c7f9bb37ab10a50a2c
Lee <al.s.apalat@gmail.com>
2017-08-08 14:51:27 +0800

    hide notif bar

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 import configureStore from './src/Store';
 import AppNavigator from './src/AppNavigator';
 
 var store = configureStore();
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={ store }>
-        <AppNavigator />
-      </Provider>
-    );
-  }
+
+	componentDidMount(){
+		StatusBar.setHidden(true);
+	}
+
+	render() {
+		return (
+			<Provider store={ store }>
+				<AppNavigator />
+			</Provider>
+		);
+	}
 }
