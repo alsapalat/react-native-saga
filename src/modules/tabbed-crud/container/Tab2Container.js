39fae5118d793f4ca9aaac8143640390ffcba171
Lee <al.s.apalat@gmail.com>
2017-08-08 15:32:50 +0800

    animation sample

--- src/modules/tabbed-crud/container/Tab2Container.js
+++ src/modules/tabbed-crud/container/Tab2Container.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Button, Animated, Easing } from 'react-native';
 
 class Tab2Container extends React.Component{
 
+	state = {
+		_toggle: false,
+		_marginTop: new Animated.Value(0),
+	}
+
+	handleToggle = () => {
+
+		const state = this.state;
+
+		this.setState({
+			_toggle: !this.state._toggle
+		}, () => {
+			Animated.timing(                  
+				state._marginTop,        
+				{
+					toValue: !state._toggle ? 100 : 0,
+					easing: Easing.bounce,
+					duration: 500,
+				}
+		    ).start();
+		})
+	}
+
+	handleChangePicker = (key) => (value, index) => {
+		this.setState({
+			[key]: value
+		})
+	}
+
+	componentDidMount(){
+		
+	}
+
 	render(){
+		const { _toggle } = this.state;
 
 		return(
 			<View>
-				<Text>//tab 2 here...</Text>
+				<Text>Animation Sample</Text>
+				<Animated.View style={{ marginTop: this.state._marginTop }}>
+					<Button title={ _toggle ? "Animate Down" : "Animate Up" } onPress={ this.handleToggle }/>
+				</Animated.View>
 			</View>
 		)
 	}
